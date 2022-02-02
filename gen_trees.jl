341857b1a9d545e30adb0535d2efa0e7b03fa7e0
Csaba Schneider <csaba.schneider@gmail.com>
2022-02-02 18:26:33 -0300

    added some functions for coloring

--- gen_trees.jl
+++ gen_trees.jl
@@ -1,7 +1,7 @@
 # This is an implementation of the algorithm to generate all trees on n vertices up to 
 # isomorphism. The algorithm is described in the paper 
 # CONSTANT TIME GENERATION OF FREE TREES*
-# ROBERT ALAN WRIGHTS’, BRUCE RICHMONDt, ANDREW ODLYZKO AND BRENDAN D. MCKAY
+# ROBERT ALAN WRIGHTS’, BRUCE RICHMONDT, ANDREW ODLYZKO AND BRENDAN D. MCKAY
 #
 # written by Csaba Schneider 
 
@@ -194,5 +194,195 @@ function Base.iterate( TI::TreeIterator, c=0 )
 end 
 
 
+function level_sequence( g::AbstractGraph )
+
+    if has_prop( g, :level_sequence )
+        return get_prop( g, :level_sequence )
+    end
+
+    vert_list = [(1,1)]
+    visited = []
+    level_seq = []
+
+    while length( vert_list ) > 0
+        (ver, current_level) = pop!( vert_list )
+        push!( visited, ver )
+        push!( level_seq, current_level )
+        append!( vert_list, 
+            reverse( [ (x,current_level+1) for x in neighbors( g, ver ) if !(x in visited) ]))
+    end 
+
+    set_prop!( g, :level_sequence, level_seq )
+    return level_seq
+end
+
+function end_of_subgraph( ls, v )
+    end_ver = findfirst( k -> k <= ls[v], ls[v+1:end] )
+    return typeof( end_ver ) == Nothing ? length( ls )-v+1 : end_ver
+end 
+
+function level_sequence_of_subgraph( ls, v )
+    
+    return ls[v:end_of_subgraph( ls, v )+v-1]
+end 
+
+function is_coloring( t, colors )
+
+    for e in edges( t )
+        if colors[src(e)] == colors[dst(e)]
+            return false
+        end 
+    end
+    return true
+end 
+
+#=
+
+function swap_coloring( t, coloring, v, w )
+
+    ev = end_of_subgraph( t, v )
+    ew = end_of_subgraph( t, w )
+    temp = coloring[v:ev]
+    coloring[v:ev] = coloring[e:ew]
+    coloring[w:ew] = temp
+end 
 
 
+function canonical_coloring( t, coloring )
+
+    ls = level_sequence( t )
+    last_ver_levels = []
+    for i in 1:length(ls)
+        
+    
+end
+  
+=#
+
+function minimal_coloring( levelseq, colors; parent_color = false )
+    
+    if parent_color isa Bool 
+        root_color = colors[1]
+    else 
+        root_color = findfirst( x -> x != parent_color, colors )
+    end 
+
+    col_even = findfirst( x -> x != root_color, colors )
+    col_odd = findfirst( x-> x != col_even, colors )
+
+    if levelseq[1] % 2 == 0 
+        col_even, col_odd = col_odd, col_even
+    end
+
+    return vcat( [root_color], 
+        [ levelseq[i] % 2 == 0 ? col_even : col_odd for i in 2:length(levelseq)])
+end
+
+function next_coloring( level_seq, coloring, colors; parent_color = colors[1]-1 )
+
+    nv = length( level_seq )
+    if nv == 1
+        next_col_pos = findfirst( x-> x > coloring[1] && x != parent_color, colors )
+        if next_col_pos isa Nothing
+            return Nothing
+        else
+            return [colors[next_col_pos]]
+        end 
+    end 
+
+    first_level = level_seq[1]
+
+    children = [ x for x in 1:nv if level_seq[x] == first_level+1 ]
+    nr_children = length( children )
+    subgraphs = [ level_sequence_of_subgraph( level_seq, c ) for c in children ]
+    children_ends = [ end_of_subgraph( level_seq, c ) + c-1 for c in children ]
+    col_children = [ coloring[children[i]:children_ends[i]] for i in 1:nr_children]
+    k = nr_children
+
+    while  k>= 0
+        c = children[k]
+
+        if k == 1 
+            new_cols = next_coloring( subgraphs[k], col_children[k], colors, 
+                                        parent_color = coloring[1] )
+            if new_cols != Nothing 
+                coloring[children[k]:children_ends[k]] = new_cols; 
+                return coloring
+            else 
+                if coloring[1] != colors[end]
+                    coloring[1] += 1
+                    if coloring[1] == parent_color 
+                        coloring[1] += 1
+                    end 
+                    if coloring[1] > colors[end] return Nothing end 
+                    for i in 1:nr_children
+                        cols = minimal_coloring( subgraphs[i], colors, parent_color = coloring[1] )
+                        coloring[children[i]:children_ends[i]] = cols  
+                    end; 
+                    return coloring
+                else
+                    return Nothing
+                end 
+            end 
+        elseif k != 1 && subgraphs[k] != subgraphs[k-1]
+            new_cols = next_coloring( subgraphs[k], col_children[k], colors,parent_color = coloring[1] )
+            if new_cols != Nothing 
+                coloring[children[k]:children_ends[k]] = new_cols; 
+                return coloring
+            else 
+                for i in k:nr_children
+                    cols = minimal_coloring( subgraphs[i], colors, parent_color = coloring[1] )
+                    coloring[children[i]:children_ends[i]] = cols  
+                end 
+                k -= 1
+            end
+        elseif k >= 2 && subgraphs[k] == subgraphs[k-1] && col_children[k] != col_children[k-1] 
+            new_cols = next_coloring( subgraphs[k], col_children[k], colors, 
+                                        parent_color = coloring[1])
+            if new_cols != Nothing 
+                coloring[children[k]:children_ends[k]] = new_cols; 
+                return coloring
+            else 
+                for i in k:nr_children
+                    cols = minimal_coloring( subgraphs[i], colors, parent_color = coloring[1] )
+                    coloring[children[i]:children_ends[i]] = cols 
+                end 
+                k = nr_children
+            end
+        else 
+            for i in k:nr_children
+                cols = minimal_coloring( subgraphs[i], colors, parent_color = coloring[1] )
+                coloring[children[i]:children_ends[i]] = cols 
+            end 
+            k -= 1 
+        end 
+    end
+    return Nothing
+end
+ 
+function all_colors( ls, colors )
+
+    colorings = [ minimal_coloring( ls, colors )]
+    while true
+        new_col = next_coloring( ls, colorings[end], colors )
+        if new_col != Nothing 
+            push!( colorings, new_col )
+        else 
+            return colorings 
+        end 
+    end 
+end 
+
+function nr_colors( ls, colors )
+
+    colorings = [ minimal_coloring( ls, colors )]
+    k = 1
+    while true
+        new_col = next_coloring( ls, colorings[end], colors )
+        if new_col == Nothing 
+            return k
+        else 
+            k += 1
+        end 
+    end 
+end 
